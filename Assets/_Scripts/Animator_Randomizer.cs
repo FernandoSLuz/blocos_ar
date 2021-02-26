63a09b7eae9cfe07ca2f03735c9a1e8637a0ffd5
FernandoSLuz <51529517+FernandoSLuz@users.noreply.github.com>
2021-02-26 09:47:02 -0300

    update

--- Assets/_Scripts/Animator_Randomizer.cs
+++ Assets/_Scripts/Animator_Randomizer.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class Animator_Randomizer : MonoBehaviour
 {
     public bool randomize_start_frame;
+    public string animation_name = "loop";
 
     private void OnEnable()
 	{
         var anim = GetComponent<Animator>();
         if (!randomize_start_frame)
         {
-            anim.Play("loop");
+            anim.Play(animation_name);
         }
         else
         {
             float startPoint = Random.Range(0f, 1f);
-            anim.Play("loop", -1, startPoint);
+            anim.Play(animation_name, -1, startPoint);
         }
     }
 }
